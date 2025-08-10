477c446d67838d42335e911e31a40fd67c3ca62e
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-08-10 20:02:15 +0530

    Create oddOrEven.js

---
+++ basic-problems/oddOrEven.js
@@ -0,0 +1,7 @@
+//Given a number n, check whether it is even or odd. Return true for even and false for odd.
+
+function oddOrEven(num){
+  return num%2 === 0 ? true:false;
+}
+
+console.log(oddOrEven());
