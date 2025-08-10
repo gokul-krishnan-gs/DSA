89bb13b2738a805c8a19d3c7c9d12f4d71e0bfdf
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-08-10 20:06:37 +0530

    Create multiplicationTable.js

---
+++ basic-problems/multiplicationTable.js
@@ -0,0 +1,9 @@
+//Given a number n, we need to print its table. 
+
+function printMulTable(num){
+  for(let i=1;i<=10;i++){
+    console.log(`${num} x ${i} = ${num*i}`);
+  }
+}
+
+printMulTable(5);
