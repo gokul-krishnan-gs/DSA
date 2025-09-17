5dac91959f8f197e20ad9a6920d96d80aa9a7aff
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-09-17 20:28:01 +0530

    Create distCandines.js

---
+++ leetCode/distCandines.js
@@ -0,0 +1,7 @@
+const distributeCandies = function(candyType) {
+    const uniqueTypes = new Set(candyType).size;
+    
+    const maxAllowed = candyType.length / 2;
+    
+    return Math.min(uniqueTypes, maxAllowed);
+};
