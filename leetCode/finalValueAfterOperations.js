b96043cdf9abec8911c6c23d697a50d401e64543
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-09-22 21:20:36 +0530

    Create finalValueAfterOperations.js

---
+++ leetCode/finalValueAfterOperations.js
@@ -0,0 +1,13 @@
+const finalValueAfterOperations = function(operations) {
+     let X = 0;
+
+    for (let op of operations) {
+        if (op.includes('++')) {
+            X += 1;
+        } else {
+            X -= 1; 
+        }
+    }
+
+    return X;   
+};
